3d586709d5c3e88e229ce5bbad6916c11a19cbef
Sitaram Rathi <srrathi2000@gmail.com>
2024-04-13 19:07:47 +0530

    added discord webhook

--- utils.js
+++ utils.js
@@ -1,4 +1,6 @@
+const { WebhookClient, EmbedBuilder } = require("discord.js");
 const Joi = require("joi");
+const { generateUsername } = require("unique-username-generator");
 
 const bodySchema = Joi.object({
     webhookUrl: Joi.string().required(),
@@ -6,11 +8,30 @@ const bodySchema = Joi.object({
     description: Joi.string()
 })
 
-const sendMessageToDiscord = async(data)=>{
+const generateEmbedding = ({ title, description }) => {
+    const embed = new EmbedBuilder()
+        .setTitle(title)
+        .setColor('#22d775')
+        .setTimestamp(new Date())
+        .setDescription(description)
+
+    return embed
+}
+
+const sendMessageToDiscord = async (data) => {
     try {
-        
+        const webhookClient = new WebhookClient({ url: data?.webhookUrl })
+
+        const embed = generateEmbedding({ title: data?.title, description: data?.description })
+        const username = generateUsername("-", 5, 15);
+
+        await webhookClient.send({
+            username: username,
+            avatarURL: `https://robohash.org/${username}`,
+            embeds: [embed]
+        })
     } catch (err) {
-        
+        console.log("error occured while calling webhook", err)
     }
 }
 
